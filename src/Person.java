bcbcb7e80048bb12743a1e1bae0603ca60518ca2
Kang01604 <salarzoncharles016@gmail.com>
2025-02-23 19:32:44 +0800

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,67 +1,61 @@
- // Define the Person class
+// Define the Person class
 public class Person {
-    // Declare a private variable to store the name of the person
+    // Declare private variables for name, age, and address
     private String name;
-    // Declare a private variable to store the age of the person
     private int age;
+    private String address;
 
-    // Constructor for the Person class that initializes the name and age variables
-    public Person(String name, int age) {
-        // Set the name variable to the provided name parameter
+    // Constructor for the Person class that initializes name, age, and address
+    public Person(String name, int age, String address) {
         this.name = name;
-        // Set the age variable to the provided age parameter
         this.age = age;
+        this.address = address;
     }
 
-    // Method to retrieve the name of the person
+    // Getter methods
     public String getName() {
-        // Return the value of the name variable
         return name;
     }
 
-    // Method to retrieve the age of the person
     public int getAge() {
-        // Return the value of the age variable
         return age;
     }
 
-    // Method to set the name of the person
+    public String getAddress() {
+        return address;
+    }
+
+    // Setter methods
     public void setName(String name) {
-        // Set the name variable to the provided name parameter
         this.name = name;
     }
 
-    // Method to set the age of the person
     public void setAge(int age) {
-        // Set the age variable to the provided age parameter
         this.age = age;
     }
-}
 
-
-// Define the Main class
-public class Main {
-    // Define the main method which is the entry point of the program
-    public static void main(String[] args) {
-        // Create an instance of the Person class with the name "Ean Craig" and age 11
-        Person person1 = new Person("Ean Craig", 11);
-        // Create another instance of the Person class with the name "Evan Ross" and age 12
-        Person person2 = new Person("Evan Ross", 12);
-
-        // Print the name and age of person1 to the console
-        System.out.println(person1.getName() + " is " + person1.getAge() + " years old.");
-        // Print the name and age of person2 to the console
-        System.out.println(person2.getName() + " is " + person2.getAge() + " years old.\n");
-
-        // Modify the age of person1 using the setter methods
-        person1.setAge(14);
-        // Modify the name and age of person2 using the setter methods
-        person2.setName("Lewis Jordan");
-        person2.setAge(12);
-        System.out.println("Set new age and name:");
-        // Print the updated name and age of person1 to the console
-        System.out.println(person1.getName() + " is now " + person1.getAge() + " years old.");
-        // Print the updated name and age of person2 to the console
-        System.out.println(person2.getName() + " is now " + person2.getAge() + " years old.");
+    public void setAddress(String address) {
+        this.address = address;
     }
-} 
+    
+   public static void main(String[] args) {
+        // Creating five Person objects
+        Person p1 = new Person("Charles", 20, "Cainta, Rizal");
+        Person p2 = new Person("Kimberly", 21, "Tagaytay, Cavite");
+        Person p3 = new Person("David", 25, "San Jose, Antique");
+        Person p4 = new Person("Evelynn", 28, "Legazpi City, Albay");
+        Person p5 = new Person("Anna", 30, "Danao City, Cebu");
+
+        // Printing their names and ages
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("|  PERSON  |       NAME      |  AGE  |        ADDRESS       |");
+        System.out.println("-------------------------------------------------------------");
+        System.out.printf("|  %-7s | %-15s |  %-4d | %-20s |\n", "1", p1.getName(), p1.getAge(), p1.getAddress());
+        System.out.printf("|  %-7s | %-15s |  %-4d | %-20s |\n", "2", p2.getName(), p2.getAge(), p2.getAddress());
+        System.out.printf("|  %-7s | %-15s |  %-4d | %-20s |\n", "3", p3.getName(), p3.getAge(), p3.getAddress());
+        System.out.printf("|  %-7s | %-15s |  %-4d | %-20s |\n", "4", p4.getName(), p4.getAge(), p4.getAddress());
+        System.out.printf("|  %-7s | %-15s |  %-4d | %-20s |\n", "5", p5.getName(), p5.getAge(), p5.getAddress());
+        System.out.println("-------------------------------------------------------------");
+
+    }
+}
